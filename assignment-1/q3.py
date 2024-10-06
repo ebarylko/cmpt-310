7e767d27df76233bcb5b524770d4372032293cb5
Eitan Barylko <eitan@barylko.com>
2024-10-05 23:16:05 -0700

    Added another derivative

--- assignment-1/q3.py
+++ assignment-1/q3.py
@@ -119,6 +119,7 @@ def d_square_loss_th(x, y, th, th0):
     """
     return -2 * (y - lin_reg(x, th, th0)) * d_lin_reg_th(x, th, th0)
 
+
 def d_mean_square_loss_th(x, y, th, th0):
     """ Returns the gradient of mean_square_loss(x, y, th, th0) with
         respect to th.  
@@ -168,7 +169,7 @@ def d_mean_square_loss_th0(x, y, th, th0):
     >>> d_mean_square_loss_th0(X, Y, th, th0).tolist()
     [[4.05]]
     """
-    return d_square_loss_th(x, y, th, th0) / x.shape[1]
+    return np.sum(d_square_loss_th0(x, y, th, th0) / x.shape[1])
 
 def d_ridge_obj_th(x, y, th, th0, lam):
     """Return the derivative of tghe ridge objective value with respect
@@ -186,8 +187,9 @@ def d_ridge_obj_th(x, y, th, th0, lam):
     >>> d_ridge_obj_th(X, Y, th, th0, 100.).tolist()
     [[210.15], [14.05]]
     """
-    #Your code here [7]
-    pass
+    number_of_columns = x.shape[1]
+    return np.sum(d_square_loss_th(x, y, th, th0) / number_of_columns, axis=1, keepdims=True) + 2 * lam * th
+
 
 def d_ridge_obj_th0(x, y, th, th0, lam):
     """Return the derivative of tghe ridge objective value with respect
@@ -205,8 +207,8 @@ def d_ridge_obj_th0(x, y, th, th0, lam):
     >>> d_ridge_obj_th0(X, Y, th, th0, 100.).tolist()
     [[4.05]]
     """
-    #Your code here [8]
-    pass
+    number_of_columns = x.shape[1]
+    return np.sum(d_square_loss_th0(x, y, th, th0) / number_of_columns, axis=1, keepdims=True)
 
 #Concatenates the gradients with respect to theta and theta_0
 def ridge_obj_grad(x, y, th, th0, lam):
--- assignment-1/test_q3.py
+++ assignment-1/test_q3.py
@@ -1,9 +1,6 @@
-import q3
 import q3 as q
-import pandas as pd
-import operator as op
-import functools as ft
-import pandas.testing as pdt
+import numpy as np
+import numpy.testing as npt
 
 # sample_data = pd.DataFrame({"mpg": [1, 2, 3, 4],
 #                             "displacement": [3, 4, 5, 6]})
@@ -26,5 +23,38 @@ import pandas.testing as pdt
 #
 # def test_one_hot():
 #     pdt.assert_frame_equal(expected_encoding, q.one_hot(sample), check_dtype=False)
+
+
+def test_mean_square_loss_th():
+    X = np.array([[ 1.,  2.,  3.,  4.], [ 1.,  1.,  1.,  1.]])
+    Y = np.array([[ 1. ,  2.2,  2.8,  4.1]])
+    th = np.array([[ 1.  ], [ 0.05]]) ; th0 = np.array([[ 2.]])
+    npt.assert_array_equal(q.d_mean_square_loss_th(X[:,0:1], Y[:,0:1], th, th0), np.array([[4.1], [4.1]]))
+
+def test_d_mean_square_loss_th0():
+    assert q.d_mean_square_loss_th0(np.array([[ 1.,  2.,  3.,  4.], [ 1.,  1.,  1.,  1.]]),
+                                    np.array([[1., 2.2, 2.8, 4.1]]),
+                                    np.array([[1.], [0.05]]),
+                                    np.array([[2.]])) == 4.05
+
+
+def test_d_ridge_obj_th():
+    X = np.array([[1., 2., 3., 4.], [1., 1., 1., 1.]])
+    Y = np.array([[1., 2.2, 2.8, 4.1]])
+    th = np.array([[1.], [0.05]]);
+    th0 = np.array([[2.]])
+    npt.assert_array_equal(q.d_ridge_obj_th(X, Y, th, th0, 0.0), np.array([[10.15], [4.05]]))
+    npt.assert_array_equal(q.d_ridge_obj_th(X, Y, th, th0, 0.5), np.array([[11.15], [4.1]]))
+    npt.assert_array_equal(q.d_ridge_obj_th(X, Y, th, th0, 100.), np.array([[210.15], [14.05]]))
+
+
+def test_d_ridge_obj_th0():
+    X = np.array([[1., 2., 3., 4.], [1., 1., 1., 1.]])
+    Y = np.array([[1., 2.2, 2.8, 4.1]])
+    th = np.array([[1.], [0.05]]);
+    th0 = np.array([[2.]])
+    npt.assert_array_equal(q.d_ridge_obj_th0(X, Y, th, th0, 0.0), np.array([[4.05]]))
+    npt.assert_array_equal(q.d_ridge_obj_th0(X, Y, th, th0, 0.5), np.array([[4.05]]))
+    npt.assert_array_equal(q.d_ridge_obj_th0(X, Y, th, th0, 100.), np.array([[4.05]]))
 
 
