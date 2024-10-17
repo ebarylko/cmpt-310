378d60122b460520b1adc08b24eafa28414bb910
Eitan Barylko <62489101+ebarylko@users.noreply.github.com>
2024-10-16 22:17:17 -0700

    Assignment 1 (#1)

---
+++ assignment-1/q2.py
@@ -0,0 +1,127 @@
+import toolz as tz
+import pandas as pd
+import numpy as np
+from scipy.stats import zscore
+import operator as op
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import f1_score
+
+
+def read_car_data(filename):
+    return pd.read_csv(filename, sep='\t')
+
+
+def filter_features(df: pd.DataFrame, wanted_features):
+    """
+    @param df: a DataFrame
+    @param wanted_features: the feature columns to keep
+    @return: a DataFrame only containing the columns in df which are in wanted_features
+    """
+    return df.filter(items=wanted_features)
+
+
+def add_mileage_label(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    @param df: a DataFrame where each row contains information about cars, such as horsepower, mpg,
+    acceleration, and more info
+    @return: adds a column to df which notes whether a given car has a poor or good mileage.
+    """
+    mpg_median = df['mpg'].median()
+    return df.assign(has_good_mpg=df['mpg'] >= mpg_median)
+
+
+def scale_features(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    @param df: a DataFrame containing information about cars from the 1970s, such as mpg,
+    acceleration, and displacement
+    @return: a DataFrame having all the features except the mileage label scaled
+    """
+    cpy = df.copy()
+    features_except_mpg, mpg_column = cpy.drop('has_good_mpg', axis="columns"), cpy['has_good_mpg']
+
+    scaled_features = zscore(features_except_mpg)
+    return pd.concat([scaled_features, mpg_column], axis=1)
+
+
+def extract_training_and_test_data(partitioned_data: list[pd.DataFrame], current_test_set):
+    """
+    @param partitioned_data: a DataFrame split into ten groups
+    @param current_test_set: the current partition of partitioned_data considered as the test data
+    and its index within the split
+    @return: the training data associated with current_test_set and the current test data
+    """
+    idx, test_data = current_test_set
+    training_data = pd.concat(partitioned_data[:idx] + partitioned_data[idx + 1:])
+    return training_data, test_data
+
+
+def generate_10_fold_cross_validation_data(data: pd.DataFrame):
+    """
+    @param data: a DataFrame where each row contains the mpg,
+    acceleration, and displacement of a car among other information
+    @return: splits the data into ten disjoint test dataset and pairs each with
+    the corresponding training dataset
+    """
+    not_empty = tz.complement(op.attrgetter("empty"))
+    split_data = list(filter(not_empty, np.array_split(data, 10)))
+
+    get_training_and_test_data = tz.partial(extract_training_and_test_data, split_data)
+    return list(map(get_training_and_test_data, enumerate(split_data)))
+
+
+@tz.curry
+def average_accuracy_and_f1_score(datasets, num_of_neighbours):
+    """
+    @param datasets: a collection of datasets, each containing training and validation data
+    number of neighbours to use for the model
+    @param num_of_neighbours: the number of neighbours to compare against
+    @return: the average accuracy and f1 score of a K-nearest neighbours model applied on each
+    dataset, where k = num_of_neighbours
+
+    """
+    def accuracy_and_f1_score(dataset):
+        """
+        @param dataset: a dataset containing training and validation data
+        @return: the accuracy and f1 score for a KNN model trained on the training data
+        and comparing with num_of_neighbours many neighbours
+        """
+        training_dataset, test_dataset = dataset
+        training_data, training_mileage_labels = (training_dataset.drop("has_good_mpg", axis='columns'),
+                                                  training_dataset['has_good_mpg'])
+
+        test_data, test_mileage_labels = (test_dataset.drop("has_good_mpg", axis='columns'),
+                                          test_dataset['has_good_mpg'])
+
+        model = (KNeighborsClassifier(n_neighbors=num_of_neighbours)
+                 .fit(training_data, training_mileage_labels))
+
+        actual_mileage_labels = model.predict(test_data)
+        return (model.score(test_data, test_mileage_labels),
+                f1_score(test_mileage_labels, actual_mileage_labels))
+
+    return tz.thread_last(datasets,
+                          (map, accuracy_and_f1_score),
+                          list,
+                          tz.partial(np.average, axis=0))
+
+
+if __name__ == '__main__':
+
+    wanted_features = ["cylinders",
+                       "displacement",
+                       "horsepower",
+                       "weight",
+                       "acceleration",
+                       "mpg"]
+
+    cross_validation_data = (read_car_data('auto-mpg-regression.tsv')
+                             .pipe(filter_features, wanted_features)
+                             .pipe(add_mileage_label)
+                             .drop('mpg', axis="columns")
+                             .pipe(scale_features)
+                             .pipe(generate_10_fold_cross_validation_data))
+
+    averages = list(map(average_accuracy_and_f1_score(cross_validation_data), [3, 6, 10, 16, 25]))
+    print(averages)
+
+
---
+++ assignment-1/test_q3.py
@@ -0,0 +1,119 @@
+import q3 as q
+import numpy as np
+import numpy.testing as npt
+
+# sample_data = pd.DataFrame({"mpg": [1, 2, 3, 4],
+#                             "displacement": [3, 4, 5, 6]})
+#
+# feature_to_scaling_function = [("mpg", ft.partial(op.add, 1)),
+#                                ("displacement", ft.partial(op.mul, 2))]
+#
+# expected_df = pd.DataFrame({"mpg": [2, 3, 4, 5],
+#                          "displacement": [6, 8, 10, 12]})
+#
+#
+# def test_normalize_and_one_hot_encode_data():
+#     pdt.assert_frame_equal(q.normalize_and_one_hot_encode_data(sample_data, feature_to_scaling_function), expected_df)
+#
+#
+# sample = pd.Series([1, 2, 3, 3])
+#
+# expected_encoding = pd.DataFrame([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 1]], columns=[1, 2, 3])
+#
+#
+# def test_one_hot():
+#     pdt.assert_frame_equal(expected_encoding, q.one_hot(sample), check_dtype=False)
+
+
+def test_mean_square_loss_th():
+    X = np.array([[ 1.,  2.,  3.,  4.], [ 1.,  1.,  1.,  1.]])
+    Y = np.array([[ 1. ,  2.2,  2.8,  4.1]])
+    th = np.array([[ 1.  ], [ 0.05]]) ; th0 = np.array([[ 2.]])
+    npt.assert_array_equal(q.d_mean_square_loss_th(X[:,0:1], Y[:,0:1], th, th0), np.array([[4.1], [4.1]]))
+
+def test_d_mean_square_loss_th0():
+    assert q.d_mean_square_loss_th0(np.array([[ 1.,  2.,  3.,  4.], [ 1.,  1.,  1.,  1.]]),
+                                    np.array([[1., 2.2, 2.8, 4.1]]),
+                                    np.array([[1.], [0.05]]),
+                                    np.array([[2.]])) == 4.05
+
+
+def test_d_ridge_obj_th():
+    X = np.array([[1., 2., 3., 4.], [1., 1., 1., 1.]])
+    Y = np.array([[1., 2.2, 2.8, 4.1]])
+    th = np.array([[1.], [0.05]]);
+    th0 = np.array([[2.]])
+    npt.assert_array_equal(q.d_ridge_obj_th(X, Y, th, th0, 0.0), np.array([[10.15], [4.05]]))
+    npt.assert_array_equal(q.d_ridge_obj_th(X, Y, th, th0, 0.5), np.array([[11.15], [4.1]]))
+    npt.assert_array_equal(q.d_ridge_obj_th(X, Y, th, th0, 100.), np.array([[210.15], [14.05]]))
+
+
+def test_d_square_loss_th0():
+    X = np.array([[ 1.,  2.,  3.,  4.], [ 1.,  1.,  1.,  1.]])
+    Y = np.array([[ 1. ,  2.2,  2.8,  4.1]])
+    th = np.array([[ 1.  ], [ 0.05]]) ; th0 = np.array([[ 2.]])
+    npt.assert_array_equal(q.d_square_loss_th0(X, Y, th, th0), np.array([[4.1, 3.6999999999999993, 4.5, 3.9000000000000004]]))
+
+def test_d_ridge_obj_th0():
+    X = np.array([[1., 2., 3., 4.], [1., 1., 1., 1.]])
+    Y = np.array([[1., 2.2, 2.8, 4.1]])
+    th = np.array([[1.], [0.05]]);
+    th0 = np.array([[2.]])
+    npt.assert_array_equal(q.d_ridge_obj_th0(X, Y, th, th0, 0.0), np.array([[4.05]]))
+    npt.assert_array_equal(q.d_ridge_obj_th0(X, Y, th, th0, 0.5), np.array([[4.05]]))
+    npt.assert_array_equal(q.d_ridge_obj_th0(X, Y, th, th0, 100.), np.array([[4.05]]))
+
+
+X = np.array([[0.0, 0.1, 0.2, 0.3, 0.42, 0.52, 0.72, 0.78, 0.84, 1.0],
+              [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]])
+y = np.array([[0.4, 0.6, 1.2, 0.1, 0.22, -0.6, -1.5, -0.5, -0.5, 0.0]])
+
+def test_d_mean_square_loss_th():
+    X = np.array([[1., 2., 3., 4.], [1., 1., 1., 1.]])
+    Y = np.array([[1., 2.2, 2.8, 4.1]])
+    th = np.array([[1.], [0.05]]);
+    th0 = np.array([[2.]])
+    npt.assert_array_equal(q.d_mean_square_loss_th(X[:,0:1], Y[:,0:1], th, th0), np.array([[4.1], [4.1]]))
+
+def J(Xi, yi, w):
+    # translate from (1-augmented X, y, theta) to (separated X, y, th, th0) format
+    return float(q.ridge_obj(Xi[:-1, :], yi, w[:-1, :], w[-1:, :], 0))
+
+
+def dJ(Xi, yi, w):
+    def f(w): return J(Xi, yi, w)
+
+    return q.num_grad(f)(w)
+
+
+def svm_min_step_size_fn(i):
+    return 0.01 / (i + 1) ** 0.5
+
+
+d, n = X.shape
+X_extend = np.vstack([X, np.ones((1, n))])
+w_init = np.zeros((d + 1, 1))
+
+print(q.sgd(X_extend, y, J, dJ, w_init, svm_min_step_size_fn, 1000)[2])
+
+def test_sgd():
+    X = np.array([[0.0, 0.1, 0.2, 0.3, 0.42, 0.52, 0.72, 0.78, 0.84, 1.0],
+                  [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]])
+    y = np.array([[0.4, 0.6, 1.2, 0.1, 0.22, -0.6, -1.5, -0.5, -0.5, 0.0]])
+
+    def J(Xi, yi, w):
+        # translate from (1-augmented X, y, theta) to (separated X, y, th, th0) format
+        return float(q.ridge_obj(Xi[:-1, :], yi, w[:-1, :], w[-1:, :], 0))
+
+    def dJ(Xi, yi, w):
+        def f(w): return J(Xi, yi, w)
+
+        return q.num_grad(f)(w)
+
+    def svm_min_step_size_fn(i):
+        return 0.01 / (i + 1) ** 0.5
+
+    d, n = X.shape
+    X_extend = np.vstack([X, np.ones((1, n))])
+    w_init = np.zeros((d+1, 1))
+    npt.assert_array_equal(q.sgd(X_extend, y, J, dJ, w_init, svm_min_step_size_fn, 1000), np.array([1, 2, 4]))
